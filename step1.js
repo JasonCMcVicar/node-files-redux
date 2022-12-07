53151e5105dbb278e27d2add199f1a1dd4c0c425
Jason <jasoncmcvicar@gmail.com>
2022-12-07 17:41:53 -0600

    finished with step 1 of exercise

--- step1.js
+++ step1.js
@@ -1,12 +1,17 @@
-const fsP = require('fs/promises')
+const fsP = require('fs/promises');
+const argv = process.argv;
 
 async function cat(){
 
   try{
-    let contents = await fsP.readFile("one.txt", "utf8");
+    let contents = await fsP.readFile(argv[2], "utf8");
     console.log("Our file contents are: ", contents);
   } catch (err) {
+    console.log(`Error reading ${argv[2]}: `);
+    console.error(err.message);
     process.exit(1);
   }
 
 }
+
+cat();
