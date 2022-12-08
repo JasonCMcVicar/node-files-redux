56571a6a912f0911b009a769ae0210eaddb21802
anela <anela89@gmail.com>
2022-12-07 16:05:34 -0800

    Created webCat function to read a URL and print to console

---
+++ step2.js
@@ -0,0 +1,41 @@
+const fsP = require('fs/promises');
+const axios = require("axios");
+const argv = process.argv;
+
+async function cat(){
+
+  try{
+    let contents = await fsP.readFile(argv[2], "utf8");
+    console.log("Our file contents are: ", contents);
+  } catch (err) {
+    console.log(`Error reading ${argv[2]}: `);
+    console.error(err.message);
+    process.exit(1);
+  }
+
+}
+
+async function webCat(url) {
+    try {
+        let resp = await axios.get(`${url}`);
+        console.log(resp.data);
+    } catch (err) {
+      console.log('inside webCat');
+        console.error(`Error fetching ${url}: ${err}`);
+        process.exit(1);
+    }
+}
+
+// store argv[2]
+// slice first 4 chars
+
+// if string equals 'http'
+    // call w/ webCat
+// call cat
+let firstFourChars = argv[2].slice(0, 4);
+
+if (firstFourChars === 'http') {
+  webCat(argv[2]);
+} else {
+  cat();
+}
